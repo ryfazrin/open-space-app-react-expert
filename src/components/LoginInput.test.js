d8916c0e04451e2693108177d520c49564788087
ryfazrin <riansera515@gmail.com>
2023-01-11 15:49:22 +0700

    created: component testing. LoginInput test

---
+++ src/components/LoginInput.test.js
@@ -0,0 +1,18 @@
+describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // Arrange
+
+    // Action
+
+    // Assert
+  });
+});
+
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle username typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
