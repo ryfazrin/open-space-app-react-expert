ab190a7a87405d3da92abcd3815980e2ceb6e199
ryfazrin <riansera515@gmail.com>
2023-01-11 16:51:53 +0700

    created: login e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login spec', () => {
+  it('should display login page correctly', () => {
+
+  });
+});
+
+/**
+ * - Login spec
+ *   - should display login page correctly
+ *   - should display alert when username is empty
+ *   - should display alert when password is empty
+ *   - should display alert when username and password are wrong
+ *   - should display homepage when username and password are correct
+ */
